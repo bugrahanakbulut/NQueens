b162b470f85db9956d66658929cb1aef105525b4
bugra <akbulutbugrahan@gmail.com>
2021-07-22 00:06:53 +0300

    Project Initialization

---
+++ NQueens/main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NQueens
+{
+    internal class main
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
